aa7ea6cda6d69f177f63afe822e5718605e1352d
wasupol_pp <wasut@bu.edu>
2020-12-25 13:07:50 -0500

    update more projects

--- src/app/accomplishments/accomplishments.component.ts
+++ src/app/accomplishments/accomplishments.component.ts
@@ -25,13 +25,17 @@ export class AccomplishmentsComponent implements OnInit {
             description: 'This project is a native Android Chat Application for course <b>CS673 at Boston University</b>. <br /> I implemented this with a native <b>Android Application framework</b>, and <b>Spring Boot Java</b> as a Backend System. Moreover, It is a chat application so to have real-time messaging. I used <b>Firebase Firestore</b> to achieve this feature.',
             imageURL: 'https://firebasestorage.googleapis.com/v0/b/wasupol-s-website.appspot.com/o/inner-circle%2FScreenshot_1607546781.png?alt=media&token=d2e65e30-0268-4bdd-81f0-696b7aad048a'
         }, {
-            name: 'Glurr',
+            name: 'Glurr (iOS Application)',
             description: 'Glurr Application is the No.1 platform for Thai university students that everyone can create any topic for freeFeatures:-Feed/Post contents about university and your interests. <br />It is developed with native <b>iOS Swift</b> and <b>PHP</b> as Backend. Here is the <a href="https://download.cnet.com/Glurr/3000-12941_4-76603491.html">download link</a>',
             imageURL: 'https://firebasestorage.googleapis.com/v0/b/wasupol-s-website.appspot.com/o/glurr%2F230x0w.png?alt=media&token=5dbf88e7-329d-4b6a-ab10-d35b4ad5b601'
         }, {
-            name: 'Glurr Talk',
+            name: 'Glurr Talk (Backend and iOS Application)',
             description: 'Glurr Talk is the #1 online live learning platform. It is developed with <b>iOS Swift</b> and <b>Node JS</b> as a backend system with <b>Firebase firestore</b> for real-time chat messaging',
             imageURL: 'https://firebasestorage.googleapis.com/v0/b/wasupol-s-website.appspot.com/o/glurr-talk%2F230x0w%20(1).png?alt=media&token=1125b73d-4bec-4c4c-a4a5-00ccfb739b1c'
+        }, {
+            name: 'TEEZY (iOS Application)',
+            description: 'TEEZY application is the customized t-shirt design and make it real. It is developed with <b>iOS Swift</b> with <b>Shopify</b> sdk.<br /> Here is the <a href="http://www.ninefar.com/?p=20430">article</a> about the application.',
+            imageURL: 'https://firebasestorage.googleapis.com/v0/b/wasupol-s-website.appspot.com/o/tezzy%2F750x750bb.jpeg?alt=media&token=b0b8dc10-2821-4245-bcfe-7925ca1433c9'
         })
     }
 
