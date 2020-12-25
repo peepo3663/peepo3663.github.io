63f84a4be6df9debdb5cde6f947b3399ad2b749f
wasupol_pp <wasut@bu.edu>
2020-12-24 23:41:27 -0500

    add inner circle project

--- src/app/accomplishments/accomplishments.component.ts
+++ src/app/accomplishments/accomplishments.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-accomplishments',
-  templateUrl: './accomplishments.component.html',
-  styleUrls: ['./accomplishments.component.css']
+    selector: 'app-accomplishments',
+    templateUrl: './accomplishments.component.html',
+    styleUrls: ['./accomplishments.component.css']
 })
 export class AccomplishmentsComponent implements OnInit {
 
-  title = 'A Few Accomplishments'
-  subtitle = 'Here are my projects and accomplishments during my working experience.'
-  accomplishments: Array<any>
-  constructor() { }
+    title = 'A Few Accomplishments'
+    subtitle = 'Here are my projects and accomplishments during my working experience.'
+    accomplishments: Array<any>
 
-  ngOnInit(): void {
-    this.accomplishments = new Array<any>()
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.accomplishments = new Array<any>()
+        this.addProject()
+    }
+
+    private addProject() {
+        this.accomplishments.push({
+            name: 'Inner circle Project (Backend and Android Application)',
+            description: 'This project is a native Android Chat Application for course <b>CS673 at Boston University</b>. <br /> I implemented this with a native Android Application framework, and Spring Boot Java as a Backend System. Moreover, It is a chat application so to have real-time messaging. I used <b>Firebase Firestore</b> to achieve this feature.',
+            imageURL: 'https://firebasestorage.googleapis.com/v0/b/wasupol-s-website.appspot.com/o/inner-circle%2FScreenshot_1607546781.png?alt=media&token=d2e65e30-0268-4bdd-81f0-696b7aad048a'
+        })
+    }
 
 }
