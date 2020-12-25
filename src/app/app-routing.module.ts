b1e2c77b0de3a24e0ee28109e27bb09577716f59
wasupol_pp <wasut@bu.edu>
2020-12-24 23:43:25 -0500

    update routing and main app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {MainComponent} from "./main/main.component";
 
 const routes: Routes = [
-  {path: '', component: MainComponent}
+  {path: '**', component: MainComponent}
 ];
 
 @NgModule({
